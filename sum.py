d4b1868173b16aba64ba55da0aaf6fbecd2697d8
Mohit <mohitt.tech@gmail.com>
2025-10-28 21:47:23 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
 # Static sum  of two numbers
-num1 = int(input("Enter first number: ")
+num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 sum = num1 + num2
 print("Sum is ", sum)
