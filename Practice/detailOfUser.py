f218a3d58055848d3c3bd3910a1a67b968f6b33f
Mohit <mohitt.tech@gmail.com>
2025-10-29 13:02:48 +0530

    Update detailOfUser.py

--- Practice/detailOfUser.py
+++ Practice/detailOfUser.py
@@ -1,5 +1,5 @@
 # Simple program to print the user's detail - taking input by user
 name = input("Enter your name :")
 age = int(input("Enter your age :"))
-salary = float(input("Enter your salary"))
+salary = float(input("Enter your salary :"))
 print("Hello I'm",name," and my age is",age, "My salary is ",salary)
