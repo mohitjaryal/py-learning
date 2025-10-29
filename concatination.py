119ea42562bbda27a012296907790efbd1ddd934
Mohit <mohitt.tech@gmail.com>
2025-10-29 13:21:01 +0530

    Update concatination.py

--- concatination.py
+++ concatination.py
@@ -2,5 +2,5 @@
 str1 = 'Mo' # String 1
 str2 = 'hit' # String 2
 result = str1+str2
-print(result) # displaying result
+print(result) # Displaying result
 print(len(result)) # Len (length) function is used toe print the length of the string in py
