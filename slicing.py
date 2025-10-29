6051d824cf15f31b1fc16b18e255b74268a119b9
Mohit <mohitt.tech@gmail.com>
2025-10-29 18:11:36 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,4 +1,4 @@
 # Program to demonstrate slicing in python
 str1 = "Hello_World"
 print("Before slicing :",str1)
-print("After slicing :",str1[6:11])
+print("After slicing :",str1[6:11]) # Slicing - from index 6 to 11 
