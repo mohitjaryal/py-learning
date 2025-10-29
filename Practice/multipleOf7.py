c3e3053c83a0e11b2feb5a9235b04e82d5b37c01
Mohit <mohitt.tech@gmail.com>
2025-10-29 22:04:38 +0530

    Update multipleOf7.py

--- Practice/multipleOf7.py
+++ Practice/multipleOf7.py
@@ -1,8 +1,6 @@
 # WAP to check that the no. entered by user is multiple of 7 or not 
 num = int(input("Enter the number :"))
 if(num % 7 == 0):
-    print(num,'is numtiple of 7')
-elif(num % 7 != 0):
-    print(num,'is not multiple of 7')
+    print(num,'is multiple of 7')
 else:
-    print("Kuch to gadbad h daya ")
+    print(num,"is not multiple of 7")
