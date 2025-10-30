4539190752fee2709d9a600d9ff4d5fdfb6d5796
Mohit <mohitt.tech@gmail.com>
2025-10-30 13:30:59 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
 # Program to demonstrate Lists in Python
-student1 = ["Mohit",99.00,12th,18,"Chandigarh"]
-student2 = ["Rohit",80.00,12th,17,"Chandigarh"]
+student1 = ["Mohit",99.00,'12th',18,"Chandigarh"] # List 1
+student2 = ["Rohit",80.00,'12th',17,"Chandigarh"] # List 2
 print(student1)
 print(student2)
