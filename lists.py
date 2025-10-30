66b878a98dd9de6a760cd45d61df7c57a8769640
Mohit <mohitt.tech@gmail.com>
2025-10-30 13:31:39 +0530

    Delete lists.py

--- lists.py
+++
@@ -1,5 +0,0 @@
-# Program to demonstrate Lists in Python
-student1 = ["Mohit",99.00,'12th',18,"Chandigarh"] # List 1
-student2 = ["Rohit",80.00,'12th',17,"Chandigarh"] # List 2
-print(student1)
-print(student2)
