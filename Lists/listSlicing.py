7777724dcd1c1501e8e2102958698565ab993dda
Mohit <mohitt.tech@gmail.com>
2025-10-30 16:48:28 +0530

    Update listSlicing.py

--- Lists/listSlicing.py
+++ Lists/listSlicing.py
@@ -1,4 +1,4 @@
 # Program to demonstrate List Slicing in Python
-str = [12,3,45,66,76,87,79]
+str = [12,3,45,66,76,87,79] # 79 is at -1 index 
 print("Before Slicing :",str)
 print("After Slicing :",str[-1:-7:-1])
