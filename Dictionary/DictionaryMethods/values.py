febf94128d398206039e867c3e6c868f1d0ec707
Mohit <mohitt.tech@gmail.com>
2025-10-30 21:59:24 +0530

    Create values.py

---
+++ Dictionary/DictionaryMethods/values.py
@@ -0,0 +1,27 @@
+# values() mehtod in py
+students = {
+    # Student 1 - Ram
+    "Ram" : {
+        'name' : 'Ram'
+        'age' : 20,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 99,
+             'chem' : 91
+        }
+    },
+
+    "Roxy" : {
+        'name' : 'Roxy',
+        'age' : 21,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 97,
+            'chem' :90
+        }
+    }
+
+}
+
+print('Ram :\n',students['Ram'].values())
+print('Roxy :\n',students['Roxy'].values())
