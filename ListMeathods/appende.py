6c80bf5516f2e64cdc744a67486009885afecb26
Mohit <mohitt.tech@gmail.com>
2025-10-30 16:55:47 +0530

    Update appende.py

--- ListMeathods/appende.py
+++ ListMeathods/appende.py
@@ -1,5 +1,6 @@
 # Program to demonstrate the use of append() meathod in Python
 num = [12,346,67,87]
 print("Before append :",num)
-num.append(90)
-print("After append :",num)
+userNum = int(input("Enter the number you want to append :"))
+num.append(userNum)
+print("After append :",userNum)
