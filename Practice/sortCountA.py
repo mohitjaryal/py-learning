9d3a3db4a1f734110975deac18c5d8fdb366625e
Mohit <mohitt.tech@gmail.com>
2025-10-30 18:08:46 +0530

    Create sortCountA.py

---
+++ Practice/sortCountA.py
@@ -0,0 +1,9 @@
+# WAP to sort tuple
+
+tup = ('A','A','S','E','R','F','D','A','A','S')
+print("Original Tuple:", tup)
+
+# Sort and convert back to tuple
+sortedTup = tuple(sorted(tup))
+
+print("Sorted Tuple:", sortedTup)
