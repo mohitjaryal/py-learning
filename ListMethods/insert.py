be8b7040756f5215576ca553850111fcf7ff5da2
Mohit <mohitt.tech@gmail.com>
2025-10-30 17:43:58 +0530

    Delete insert.py

--- ListMethods/insert.py
+++
@@ -1,8 +0,0 @@
-# insert() method in py
-num = [1,2,3,4,6]
-print("Before inserting",num)
-idx = int(input("Enter the index number :"))
-val = input("Enter the value :")
-
-newIdx = num[:idx] + [val] + num[idx:] # adding new value to the desired index
-print("After inserting :",newIdx)
