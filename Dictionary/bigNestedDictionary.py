671af1db60915fbbd857c699f930cb5691be1844
Mohit <mohitt.tech@gmail.com>
2025-10-30 21:54:01 +0530

    Update bigNestedDictionary.py

--- Dictionary/bigNestedDictionary.py
+++ Dictionary/bigNestedDictionary.py
@@ -8,6 +8,18 @@ students = {
             'phy' : 100,
             'math' : 99
         }
+    },
+
+    "Roxy" : {
+        'name' : 'Roxy',
+        'age' : 21,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 97
+        }
     }
 
 }
+
+print('Ram :\n',students['Ram'])
+print('Roxy :\n',students['Roxy'])
