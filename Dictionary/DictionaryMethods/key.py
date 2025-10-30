be61f7a306a4cde5c5ead87a4d0fe3c28b520f18
Mohit <mohitt.tech@gmail.com>
2025-10-30 21:56:23 +0530

    Create key.py

---
+++ Dictionary/DictionaryMethods/key.py
@@ -0,0 +1,27 @@
+# key() method in py
+
+# Nested dictionary 
+students = {
+    # Student 1 - Ram
+    "Ram" : {
+        'name' : 'Ram',
+        'age' : 20,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 99
+        }
+    },
+
+    "Roxy" : {
+        'name' : 'Roxy',
+        'age' : 21,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 97
+        }
+    }
+
+}
+
+print('Ram :\n',students['Ram'].keys())
+print('Roxy :\n',students['Roxy'].keys())
