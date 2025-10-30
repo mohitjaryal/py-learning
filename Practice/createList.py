21119e975b4fae49e6704c24606c87516e900d05
Mohit <mohitt.tech@gmail.com>
2025-10-30 17:54:37 +0530

    Update createList.py

--- Practice/createList.py
+++ Practice/createList.py
@@ -1,3 +1,3 @@
-# WAP to ask user about his 3 fav movies and make a list
+# WAP to ask user about his 3 fav movies and make a list 
 movies = list(input("Enter your three favourite movies :").split())
 print("Your favourite movies are :",movies)
