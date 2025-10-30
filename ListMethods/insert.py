a080bd84e255d99b36b7296308f2a6093f28e092
Mohit <mohitt.tech@gmail.com>
2025-10-30 17:12:56 +0530

    Update insert.py

--- ListMethods/insert.py
+++ ListMethods/insert.py
@@ -3,3 +3,6 @@ num = [1,2,3,4,6]
 print("Before inserting",num)
 idx = int(input("Enter the index number :"))
 val = int(input("Enter the value :"))
+
+newIdx = num[:idx] + val + num[idx:] # adding new value to the desired index
+print("After inserting :",newIdx)
