dd25a37e75987fa51c764b06a0ee5b5176e31862
Mohit <mohitt.tech@gmail.com>
2025-10-30 17:44:02 +0530

    Delete appende.py

--- ListMethods/appende.py
+++
@@ -1,6 +0,0 @@
-# Program to demonstrate the use of append() meathod in Python
-num = [12,346,67,87]
-print("Before append :",num)
-val = int(input("Enter the number you want to append :"))
-num.append(val)
-print("After append :",num)
