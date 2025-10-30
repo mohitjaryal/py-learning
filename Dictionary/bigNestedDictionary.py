bc6805ca0608a0d1b478b46db5c0712b82190bc8
Mohit <mohitt.tech@gmail.com>
2025-10-30 21:51:47 +0530

    Create bigNestedDictionary.py

---
+++ Dictionary/bigNestedDictionary.py
@@ -0,0 +1,13 @@
+# Nested dictionary 
+students = {
+    # Student 1 - Ram
+    "Ram" : {
+        'name' : 'Ram',
+        'age' : 20,
+        'subjects' : {
+            'phy' : 100,
+            'math' : 99
+        }
+    }
+
+}
