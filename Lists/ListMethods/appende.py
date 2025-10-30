2385f04d9f9cd283b7101d0f44a03d9cc18387c7
Mohit <mohitt.tech@gmail.com>
2025-10-30 17:43:41 +0530

    Create appende.py

---
+++ Lists/ListMethods/appende.py
@@ -0,0 +1,6 @@
+# Program to demonstrate the use of append() meathod in Python
+num = [12,346,67,87]
+print("Before append :",num)
+val = int(input("Enter the number you want to append :"))
+num.append(val)
+print("After append :",num)
