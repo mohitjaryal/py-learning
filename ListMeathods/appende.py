922189c906941da8f1d1f20d1b2529f2bfb482ef
Mohit <mohitt.tech@gmail.com>
2025-10-30 16:54:01 +0530

    Update appende.py

--- ListMeathods/appende.py
+++ ListMeathods/appende.py
@@ -1,4 +1,4 @@
 # Program to demonstrate the use of append() meathod in Python
-list = [12,346,67,87]
-print("Before append :",list)
-print(list.append(90))
+num = [12,346,67,87]
+print("Before append :",num)
+print(num.append(90))
