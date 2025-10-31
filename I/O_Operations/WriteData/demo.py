9d33f60769971ef2958a0dfae168b6e64607b21d
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:42:09 +0530

    Update demo.py

--- I/O_Operations/WriteData/demo.py
+++ I/O_Operations/WriteData/demo.py
@@ -1,1 +1,6 @@
  # Basic program to demonstrate the I/O operations in python
+
+f = open('/Users/mohit/Documents/py-learning/I/O_Operations/WriteData/demo.txt','r')
+
+data = f.write("File data is changed ") # overwrites the entire file
+print(data)
