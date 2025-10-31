8d3afdf26abf755e9b8301bb912181441e19a8fc
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:29:47 +0530

    Update readline.py

--- I/O_Operations/readline.py
+++ I/O_Operations/readline.py
@@ -1,5 +1,14 @@
 # readline() I/O operation in py
 f = open('/Users/mohit/Documents/py-learning/I/O_Operations/demo.txt','r')
 
+# Reading and printing line1 - 'Hello world'
+line1 = f.readline()
+print(line1)
+
+# Reading and printing line2
+line1 = f.readline()
+print(line1)
+
+# Readinf and printing line3
 line1 = f.readline()
 print(line1)
