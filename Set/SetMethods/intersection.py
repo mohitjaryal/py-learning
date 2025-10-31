2650fb675d5f574c2ddfcd5a2a813af6558f3c7e
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:24:31 +0530

    Update intersection.py

--- Set/SetMethods/intersection.py
+++ Set/SetMethods/intersection.py
@@ -1,5 +1,5 @@
 # intersection() method in py
-set1 = {1,2,2,3,4,5}
-set2 = {1,1,2,4,5,5}
+set1 = {2,3,4,5}
+set2 = {1,2,3,4,5,6,7}
 print("Before intersection :",set1,set2)
 print("After intersection :",set1.intersection(set2)) # This will create a new set and doesn't change the exixting set1 & set2
