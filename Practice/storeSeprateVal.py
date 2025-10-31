a0188820b6b800e5c45f6330d6f777536f01a3a2
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:49:42 +0530

    Update storeSeprateVal.py

--- Practice/storeSeprateVal.py
+++ Practice/storeSeprateVal.py
@@ -1,6 +1,6 @@
 # WAP to store seprate value - 9 & 9.0 in a set (using built in data types)
 values ={
-    ('int', 9) # Created a tuple 
+    ('int', 9), # Created a tuple 
      ('float', 9.0) # Created a tuple 
 }
 
