d83c356f240b9dcec25db2be4c5003d495d752f7
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:42:42 +0530

    Create marksDictionary.py

---
+++ Practice/marksDictionary.py
@@ -0,0 +1,16 @@
+# WAP to enter the marks of 3 subjects from user and store it in a dictionary
+marks = {} # Created an empty dictionary
+
+a = int(input("Enter marks of phy:"))
+marks.update({'phy':a}) # Updating the marks dictionary 
+
+b =int(input("Enter marks of math:")) # Updating the marks dictionary 
+marks.update({'phy':b})
+
+c = int(input("Enter marks of psychology:")) # Updating the marks dictionary 
+marks.update({'phy':c})
+
+d = int(input("Enter marks of chem:")) # Updating the marks dictionary 
+marks.update({'phy':d})
+
+print('Detailed marks :',marks)
