db81498cca305d4ad733bb0d3cd47ea121a2e0f6
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:45:43 +0530

    Delete demo.py

--- I/O_Operations/WriteData/demo.py
+++
@@ -1,7 +0,0 @@
- # Basic program to demonstrate the I/O operations in python
-
-f = open("/Users/mohit/Documents/py-learning/I/O_Operations/WriteData/demo.txt","w") 
-
-f.write("File changed !")
-
-f.close() # Closing the file
