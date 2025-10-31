6154b80a347f002d652ceaf3abfd4974588f0bd4
Mohit <mohitt.tech@gmail.com>
2025-10-31 11:59:03 +0530

    Create arithmetic.py

---
+++ Practice/arithmetic.py
@@ -0,0 +1,27 @@
+# WAP to perform basic arithmetic operations using differnet functions
+
+# sub() function - perform addition
+def sum(a,b):
+    res = a + b
+    print(res)
+    return(res)
+
+# sub() - perform subtraction
+def sub(c,d):
+    result = c - d
+    print(result)
+    return(result)
+
+
+# sub() - perform subtraction
+def mul(e,f):
+    ans = e - f
+    print(ans)
+    return(ans)
+
+
+# sub() - perform subtraction
+def div(g,h):
+    final = g - h
+    print(final)
+    return(final)
