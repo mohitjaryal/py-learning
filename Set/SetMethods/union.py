0eb3549e1f1f300145d7c511997a54b2d92a3d09
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:20:46 +0530

    Update union.py

--- Set/SetMethods/union.py
+++ Set/SetMethods/union.py
@@ -2,4 +2,4 @@
 set1 = {1,2,2,3,4,5}
 set2 = {1,1,2,4,5,5}
 print("Before union :",set1,set2)
-print("After union :",set1.union(set2))
+print("After union :",set1.union(set2)) # This will create a new set and doesn't change the exixting set1 & set2
