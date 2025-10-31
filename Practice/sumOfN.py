281615d58587f095d8ab07d1385fc353218289e8
Mohit <mohitt.tech@gmail.com>
2025-10-31 11:37:17 +0530

    Create sumOfN.py

---
+++ Practice/sumOfN.py
@@ -0,0 +1,8 @@
+# WAP to find sum of n numbers using while() loop
+inp = int(input("Enter the number :"))
+i = 1
+result = 0
+while(i <inp):
+    result += inp
+    i +=1
+print("Sum is :",result)   
