bed51df56c559fa5c9656fdfb4048e8f4151e43f
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:25:01 +0530

    Update demo.py

--- I/O_Operations/demo.py
+++ I/O_Operations/demo.py
@@ -1,6 +1,8 @@
 # Basic program to demonstrate the I/O operations in python
 f = open("/Users/mohit/Documents/py-learning/I/O_Operations/demo.txt","r")
-data = f.read() # Reading the data
+
+data = f.read() # Reading the data - entire data
+data = f.read(5) # Reading first 5 letter or char 
 print(data) # Printing the data
 print(type(data))
 
