b98374442f0924fda68013f9dd8054de8189e745
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:23:08 +0530

    Create intersection.py

---
+++ Set/SetMethods/intersection.py
@@ -0,0 +1,5 @@
+# intersection() method in py
+set1 = {1,2,2,3,4,5}
+set2 = {1,1,2,4,5,5}
+print("Before intersection :",set1,set2)
+print("After intersection :",set1.intersection(set2)) # This will create a new set and doesn't change the exixting set1 & set2
