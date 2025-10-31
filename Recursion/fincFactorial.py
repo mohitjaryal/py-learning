099a50c1013f2f6dd75c82c6449abc489836e0a8
Mohit <mohitt.tech@gmail.com>
2025-10-31 21:52:29 +0530

    Update fincFactorial.py

--- Recursion/fincFactorial.py
+++ Recursion/fincFactorial.py
@@ -1,8 +1,8 @@
 # Write a recursive function to calculate the factorial 
 def fac(n):
-    if(n == 0 or n == 1):
+    if(n == 0 or n == 1): # Base case
         return 1
     else:
-        return n * fac(n-1)
+        return n * fac(n-1) # Final argument
     
 print(fac(5))
