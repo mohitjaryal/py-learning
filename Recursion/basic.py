5bda9ad1de1469e7e2438f58493029ea53ce6852
Mohit <mohitt.tech@gmail.com>
2025-10-31 12:34:58 +0530

    Create basic.py

---
+++ Recursion/basic.py
@@ -0,0 +1,11 @@
+# Basic demonstration of recursion in py
+def show(n):
+    if n == 0:
+        print(n)
+        return    # stop recursion
+    else:
+        print(n)
+        show(n - 1)   # recursive call
+
+
+show(4)
