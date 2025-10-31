9760c2005567cb166eac1fcb3b28cd147ab37d0a
Mohit <mohitt.tech@gmail.com>
2025-10-31 12:27:00 +0530

    Create elementInList.py

---
+++ Practice/elementInList.py
@@ -0,0 +1,9 @@
+# WAF to print element s list
+val = ['hye','bye','veryGood','veryBad','hye','bye','veryGood','veryBad','hye','bye','veryGood','veryBad']
+
+# Finction
+def elements(list):
+    for items in list:
+        print(items)
+
+elements(val)
