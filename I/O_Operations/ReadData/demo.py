43bf2e474ed14246e4482b635d3d2f4826bbcb06
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:35:30 +0530

    Create demo.py

---
+++ I/O_Operations/ReadData/demo.py
@@ -0,0 +1,10 @@
+# Basic program to demonstrate the I/O operations in python
+f = open("/Users/mohit/Documents/py-learning/I/O_Operations/demo.txt","r")
+
+data = f.read() # Reading the data - entire data
+# data = f.read(5) # Reading first 5 letter or char 
+print(data) # Printing the data
+print(type(data))
+
+f.close() # Closing the file
+print("File closed ")
