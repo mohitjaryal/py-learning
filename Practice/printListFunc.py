416c2c7f37f153175cfac4128066d057ba9f16da
Mohit <mohitt.tech@gmail.com>
2025-10-31 12:22:18 +0530

    Create printListFunc.py

---
+++ Practice/printListFunc.py
@@ -0,0 +1,10 @@
+# WAF to print the length of the list in the parameter
+
+val = ['hello','hye','bye','veryGood','veryBad']
+val2 = ['hello','hye','bye','veryGood','veryBad','hye','bye','veryGood','veryBad','hye','bye','veryGood','veryBad','hye','bye','veryGood','veryBad']
+# Function to print the length of list 
+def lenth(list):
+    print(len(list))
+
+lenth(val)
+lenth(val2)
