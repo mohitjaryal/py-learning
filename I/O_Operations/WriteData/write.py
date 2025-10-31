5a74658fdce8d6c225e2bb082d429254b477c81f
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:45:46 +0530

    Create write.py

---
+++ I/O_Operations/WriteData/write.py
@@ -0,0 +1,7 @@
+ # Basic program to demonstrate the I/O operations in python
+
+f = open("/Users/mohit/Documents/py-learning/I/O_Operations/WriteData/demo.txt","w") 
+
+f.write("File changed !")
+
+f.close() # Closing the file
