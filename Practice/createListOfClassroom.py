04ae01c0e9996eea06fd10444b2531cb714d9d44
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:34:11 +0530

    Update createListOfClassroom.py

--- Practice/createListOfClassroom.py
+++ Practice/createListOfClassroom.py
@@ -1,1 +1,3 @@
 # WAP to calculate how many classrooms are required by all students
+classroom = ['pthon','java','c++','python','javascript','java','python','java','c++','c']
+print("Total classroom required :",len(classroom))
