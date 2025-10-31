fd4c1ba1ecf88f1eb443f2b5d59ef60332eaff72
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:16:28 +0530

    Update demo.py

--- I/O_Operations/demo.py
+++ I/O_Operations/demo.py
@@ -1,4 +1,8 @@
 # Basic program to demonstrate the I/O operations in python
-f = open("demo.txt","r")
+f = open("/Users/mohit/Documents/py-learning/I/O_Operations/demo.txt","r")
 data = f.read() # Reading the data
 print(data) # Printing the data
+print(type(data))
+
+f.close() # Closing the file
+print("File closed ")
