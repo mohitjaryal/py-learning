4c7d3e7e1181073a10db0024801dc268705fa3cd
Mohit <mohitt.tech@gmail.com>
2025-10-31 09:29:59 +0530

    Create createDictionary.py

---
+++ Practice/createDictionary.py
@@ -0,0 +1,8 @@
+# WAP to create a Dictionary to store word meanings in py dict
+dict = {
+    'table' :  ['a piece of furniture', 'list of fact and figure'],
+    'cat' : 'a small animal'
+}
+
+print('Table :',dict['table'])
+print('Cat :',dict['cat'])
