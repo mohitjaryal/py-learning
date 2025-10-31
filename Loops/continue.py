ac59d2eb448e3c5f9f58725aeaece6ab72963780
Mohit <mohitt.tech@gmail.com>
2025-10-31 10:47:10 +0530

    Create continue.py

---
+++ Loops/continue.py
@@ -0,0 +1,10 @@
+# continue statement in oy
+# break statement in oy
+i = 1
+while i <=10:
+    print(i)
+    if(i==6):
+        continue # skips 6 print the rest 
+    i += 1
+    
+print("End of loop !")
