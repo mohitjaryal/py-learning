cedc90138fb494529c333b496a48f28dec1ccbeb
Mohit <mohitt.tech@gmail.com>
2025-11-01 02:25:41 +0530

    Update demo.py

--- I/O_Operations/demo.py
+++ I/O_Operations/demo.py
@@ -2,7 +2,7 @@
 f = open("/Users/mohit/Documents/py-learning/I/O_Operations/demo.txt","r")
 
 data = f.read() # Reading the data - entire data
-data = f.read(5) # Reading first 5 letter or char 
+# data = f.read(5) # Reading first 5 letter or char 
 print(data) # Printing the data
 print(type(data))
 
