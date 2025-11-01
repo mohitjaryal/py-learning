7b1db8f2ac573b9510a393654ff03d239689d9c4
Mohit <mohitt.tech@gmail.com>
2025-11-01 13:46:18 +0530

    Update removeJava.py

--- Practice/I/O/removeJava.py
+++ Practice/I/O/removeJava.py
@@ -1,1 +1,9 @@
 # WAF tp remove java from practice.txt 
+# Reading data
+with open("/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt", 'r') as f:
+    data = f.read()
+
+data.replace("java")
+
+with open("/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt", 'r') as f:
+    f.write()
