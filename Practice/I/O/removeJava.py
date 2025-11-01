591b7bbd748e7ab169d20b79b47e28cb86ff5c03
Mohit <mohitt.tech@gmail.com>
2025-11-01 13:50:57 +0530

    Update removeJava.py

--- Practice/I/O/removeJava.py
+++ Practice/I/O/removeJava.py
@@ -2,8 +2,7 @@
 # Reading data
 with open("/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt", 'r') as f:
     data = f.read() # Reading the file
-    print("Old data :\n",data)
 
-newData = data.replace("Java","HTML")
+newData = data.replace("Java","Hello") # Replacing the 'Java' with 'Hello'
 print(newData) # Displaying newData
 f.close() # Closing the file
