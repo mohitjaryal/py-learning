77553b93786d0fb9b1031dc0e280e49b4a16e5b6
Mohit <mohitt.tech@gmail.com>
2025-11-01 13:38:11 +0530

    Update wMode.py

--- Practice/I/O/wMode.py
+++ Practice/I/O/wMode.py
@@ -1,6 +1,6 @@
 # WAP to create a file using py and write data in it
 
-with open("practice.txt",'+w') as f:
+with open("/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt",'+w') as f:
     f.write("I have created a ne file !")
     data = f.read()
     print(data)
