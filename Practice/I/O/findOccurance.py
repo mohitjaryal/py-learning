63c028bcd3c5262c16aec3eb21affc400a650128
Mohit <mohitt.tech@gmail.com>
2025-11-01 14:12:00 +0530

    Update findOccurance.py

--- Practice/I/O/findOccurance.py
+++ Practice/I/O/findOccurance.py
@@ -1,6 +1,6 @@
 # WAF to find the first occurance of word 'Java' in practice.txt
 
-def checkWord();
+def checkWord():
     word = 'java'
     with open('/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt','r') as f:
         data = f.read() # Reading the data
@@ -8,3 +8,11 @@ def checkWord();
             print('Found')
         else:
             print("Not found")
+
+# Function to check for the line
+def checkline():
+     word = 'java'
+     data = True
+     with open('/Users/mohit/Documents/py-learning/Practice/I/O/practice.txt','r') as f:
+         while data: # while data means - do work till data (variable name) has valid data 
+             data = f.readline()
