b18321963c40fa39a0cd4476039641539e937b2d
Mohit <mohitt.tech@gmail.com>
2025-11-01 13:25:29 +0530

    Update withSyntax.py

--- I/O_Operations/WithSyntax/withSyntax.py
+++ I/O_Operations/WithSyntax/withSyntax.py
@@ -1,1 +1,6 @@
 # with syntax in py to perform i/o operations
+ 
+# with syntax
+with open("/Users/mohit/Documents/py-learning/I/O_Operations/WithSyntax/demo.txt","r") as f:
+    data = f.read()
+    print(data)
