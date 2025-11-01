092fb8e115c9c0029c8f4f0133d3a31a3cb9da2d
Mohit <mohitt.tech@gmail.com>
2025-11-01 14:38:26 +0530

    Update counEven.py

--- Practice/I/O/counEven.py
+++ Practice/I/O/counEven.py
@@ -1,9 +1,11 @@
-# Form a file containing numbers and find the count of all even numbers
+# Create a file containing numbers and find the count of all even numbers (total occurance)
+
 # Creating file
 with open("/Users/mohit/Documents/py-learning/Practice/I/O/evenCount.txt", 'w') as f:
     f.write("1,2,3,4,5,6,7,8,9") # Created a file
 
 # Reading and finding data
+count = 0
 with open("/Users/mohit/Documents/py-learning/Practice/I/O/evenCount.txt", 'r') as f:
     data = f.read()
     print(data) # printing the data
@@ -18,4 +20,8 @@ with open("/Users/mohit/Documents/py-learning/Practice/I/O/evenCount.txt", 'r') 
 
     # Using split() method 
     num = data.split(',')
-    print(num)
+    for val in num:
+        if(int(val) %2 == 0): # casting val into int 
+            count += 1
+
+print(count) # Displaying count 
