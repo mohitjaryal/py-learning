444e43bdf682800de781c2f291c560a32b833b4c
Mohit <mohitt.tech@gmail.com>
2025-11-01 14:25:51 +0530

    Update counEven.py

--- Practice/I/O/counEven.py
+++ Practice/I/O/counEven.py
@@ -1,3 +1,3 @@
 # Form a file containing numbers and find the count of all even numbers
-with open("/Users/mohit/Documents/py-learning/Practice/I/O", 'w') as f:
+with open("/Users/mohit/Documents/py-learning/Practice/I/O/evenCount.txt", 'w') as f:
     f.write("1,2,3,4,5,6,7,8,9") # Created a file
