e19efe373a590c3aac385b5a1845fb30c9574bb5
Mohit <mohitt.tech@gmail.com>
2025-11-01 20:20:57 +0530

    Update studentMarks.py

--- Practice/OOP/studentMarks.py
+++ Practice/OOP/studentMarks.py
@@ -1,13 +1,22 @@
-# Create a student class that takes name & marks of 3 subjects as argument in constructor then create a method & print 
+# Create a student class that takes name & marks of 3 subjects as argument in constructor then create a method to find average & print
 
 # Student class
-
 class Student:
     # Constructor
-    def __init__(self,name,marks):
-       self.name = name
-       self.marks = marks
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
 
-    # Method
+    # Method to calculate average
+    def avg(self):
+        total = 0
+        for val in self.marks:
+            total += val
+        print('Hi', self.name, 'your average score is:', total / 3)
 
-s1 = Student('Mohit',[98,99,100])
+
+# Creating object
+s1 = Student('Mohit', [98, 99, 100])
+
+# Calling method
+s1.avg()
