d720dee3b553045870bf04419973cc0b0118224d
Mohit <mohitt.tech@gmail.com>
2025-11-01 14:22:38 +0530

    Update findOccurance.py

--- Practice/I/O/findOccurance.py
+++ Practice/I/O/findOccurance.py
@@ -1,25 +1,25 @@
 def checkWord():
-    word = 'java'
+    word = 'Java'
     with open('/Users/mohit/Documents/py-learning/Practice/I/O/findOccurance.txt','r') as f:
         data = f.read() # Reading the data
         if(word in data):
-            print('Found')
+            print('Word Found\n')
         else:
             print("Not found")
 
 # Function to check for the line
 def checkline():
-    word = 'java'
+    word = 'Java'
     data = True 
     lineNo = 1 # line no.
     with open('/Users/mohit/Documents/py-learning/Practice/I/O/findOccurance.txt','r') as f:
         while data: # while data means - do work till data (variable name) has valid data 
             data = f.readline()
             if(word in data):
-                print(lineNo)
+                print('Found at line :',lineNo)
                 return  # stop after finding the first occurrence
             lineNo += 1  # updating line no.
     return -1 # if the word doesn't exist 
 
-checkline() # function call
 checkWord() # function call
+checkline() # function call
