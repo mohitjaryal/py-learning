869005a978ce63285ec704c9fb8652ae2a15f163
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:16:35 +0530

    Update car.py

--- OOP/Inheritance/car.py
+++ OOP/Inheritance/car.py
@@ -4,4 +4,15 @@ class Car:
     @staticmethod
     def start():
         print('Car started ')
-        
+
+    @staticmethod
+    def stop():
+        print('Car stopped ')
+
+class Toyota:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+
+t1 = Toyota('Toyota','Fortuner')
