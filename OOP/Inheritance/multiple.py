5ebc5d2bdd64978d47f8f606622d4eb9c1b166cd
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:32:29 +0530

    Update multiple.py

--- OOP/Inheritance/multiple.py
+++ OOP/Inheritance/multiple.py
@@ -1,1 +1,12 @@
 # Multiple inheritance
+
+class Parent1:
+    hello = 'Hello from parent 1'
+
+class Parent2:
+    hello = 'Hello from parent 2'
+
+class Child(Parent1,Parent2):
+    hello = 'Hello form Child'
+
+c1 = Child() # Object of Child class
