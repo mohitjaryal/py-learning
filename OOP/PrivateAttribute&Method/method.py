4ef7fa3d961af1411aef9b58256a6a80400625d1
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:11:41 +0530

    Update method.py

--- OOP/PrivateAttribute&Method/method.py
+++ OOP/PrivateAttribute&Method/method.py
@@ -2,8 +2,12 @@
 
 class Person:
     # Created a private hello method (we can make any method private by using __)
-    def __hello():
+    def __hello(self):
         print('Hello') 
 
+    def welcome(self):
+        self.__hello()
+
 p1 = Person()
+print(p1.welcome()) # Calling hello() method 
 # print(p1.__hello()) # This will not work because the __hello method is private and can only be call inside the Person class 
