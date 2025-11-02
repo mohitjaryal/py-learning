b9b99c48ec36e7dee65483934ea81374b3c28099
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:33:42 +0530

    Update multiple.py

--- OOP/Inheritance/multiple.py
+++ OOP/Inheritance/multiple.py
@@ -1,12 +1,15 @@
 # Multiple inheritance
 
 class Parent1:
-    hello = 'Hello from parent 1'
+    hello1 = 'Hello from parent 1'
 
 class Parent2:
-    hello = 'Hello from parent 2'
+    hello2 = 'Hello from parent 2'
 
 class Child(Parent1,Parent2):
-    hello = 'Hello form Child'
+    hello3 = 'Hello form Child'
 
 c1 = Child() # Object of Child class
+print(c1.hello1)
+print(c1.hello2)
+print(c1.hello3)
