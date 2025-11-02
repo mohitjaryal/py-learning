af5006e376680c33b63d2950a5b2053814c20972
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:27:18 +0530

    Update multiLevel.py

--- OOP/Inheritance/multiLevel.py
+++ OOP/Inheritance/multiLevel.py
@@ -10,14 +10,26 @@ class Car:
     def stop():
         print('Car stopped')
 
-# Child class
+# Child class1
 class Toyota(Car):  # Inheriting from Car (Parent class)
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+# Child class
+class BMW(Car):  # Inheriting from Car (Parent class)
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
 
 
 t1 = Toyota('Toyota', 'Fortuner')
+t1.start()   #  Accessing parent start() method
+print(t1.brand)
+print(t1.model)
+t1.stop()    # Accessing parent stop() method
+
+b1 = BMW('BMW','M5')
 t1.start()   #  Accessing parent start() method
 print(t1.brand)
 print(t1.model)
