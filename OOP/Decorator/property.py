e06126e5dba0676680a1be7cf3df2bcee4a87a56
Mohit <mohitt.tech@gmail.com>
2025-11-02 15:44:57 +0530

    Update property.py

--- OOP/Decorator/property.py
+++ OOP/Decorator/property.py
@@ -1,1 +1,21 @@
 # @property decorator in py
+
+class Student:
+    # Constructor
+    def __init__(self,math,phy,chem):
+        self.math  = math
+        self.phy = phy
+        self.chem = chem
+
+    # Average
+    @property
+    def avg(self):
+        return str((self.math + self.phy + self.chem ) / 3 )
+    
+
+s1 = Student(100,99,100) # object
+print('Old avg :',s1.avg)
+
+# Changing marks
+s1.chem = 91
+print('New avg :',s1.avg)
