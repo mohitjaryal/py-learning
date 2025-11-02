fce6740329bdbb064bddfa0abfbfcecf3b4e9359
Mohit <mohitt.tech@gmail.com>
2025-11-02 14:03:52 +0530

    Update attribute.py

--- OOP/PrivateAttribute&Method/attribute.py
+++ OOP/PrivateAttribute&Method/attribute.py
@@ -1,5 +1,10 @@
 # Private attribute in py
 class Account:
     def __init__(self,acc,userName):
-        self.__acc_no = acc
+        self.__acc_no = acc # created a private acc_no attribute
         self.userName = userName
+
+acc1 = Account('12345','Mohit')
+
+# print(acc1.__acc_no) # this will not print because acc_no is private attribute
+print(acc1.userName)
