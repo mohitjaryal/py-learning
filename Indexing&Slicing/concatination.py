1c940b891e6bae09c37a81e9db932546f24e358b
Mohit <mohitt.tech@gmail.com>
2025-11-27 01:53:16 +0530

    Create concatination.py

---
+++ Indexing&Slicing/concatination.py
@@ -0,0 +1,6 @@
+# Basic program to show Concatination in Python
+str1 = 'Mo' # String 1
+str2 = 'hit' # String 2
+result = str1+str2
+print(result) # Displaying result
+print(len(result)) # Len (length) function is used toe print the length of the string in py
