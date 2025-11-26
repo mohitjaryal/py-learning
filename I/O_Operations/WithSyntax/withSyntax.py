bceed15bbde98d7241adcaf438bb9576d1b26abd
Mohit <mohitt.tech@gmail.com>
2025-11-27 02:01:07 +0530

    Delete withSyntax.py

--- I/O_Operations/WithSyntax/withSyntax.py
+++
@@ -1,7 +0,0 @@
-# with syntax in py to perform i/o operations
- 
-# with syntax
-with open("/Users/mohit/Documents/py-learning/I/O_Operations/WithSyntax/demo.txt","r") as f:
-    data = f.read() # Reading data
-    print(data) # Printing data
-    f.close() # Closing file
