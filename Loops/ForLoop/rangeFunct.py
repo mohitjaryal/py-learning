39b31353ec0828881982db5530ee8055e83ae5d9
Mohit <mohitt.tech@gmail.com>
2025-12-08 23:05:09 +0530

    Update rangeFunct.py

--- Loops/ForLoop/rangeFunct.py
+++
@@ -1,5 +0,0 @@
-# range() function in py
-# for el in range(5):  # printing range of 5
-#  print(el)
-for el in range(5,19,2): # 5 - start, 19 - stop, 2 - step (means the increment order)
-    print(el)
