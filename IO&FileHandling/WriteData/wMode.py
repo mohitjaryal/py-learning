9c7d68e2bfd07a5f505d36afaca597ae6cd2dbde
Mohit <mohitt.tech@gmail.com>
2025-12-08 23:03:07 +0530

    Update wMode.py

--- IO&FileHandling/WriteData/wMode.py
+++
@@ -1,7 +0,0 @@
- # Basic program to demonstrate the write operation in python using 'w' mode - overwrites all data
-
-f = open("/Users/mohit/Documents/py-learning/I/O_Operations/WriteData/demo.txt","w") 
-
-f.write("File changed !")
-
-f.close() # Closing the file
